c13f564f8460cf4ba540942095deb1c19b9ad310
Quinten Van Kerrebroeck <quintenvk@gmail.com>
2014-02-06 13:37:17 +0100

    fix repo conflict

--- src/Hash.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace quintenvk;
-class Hash {
-	public static function generateSalt($max = 15) {
-		mt_srand(microtime(true)*100000 + memory_get_usage(true));
-		return md5(uniqid(mt_rand(), true));
-	}
-}
